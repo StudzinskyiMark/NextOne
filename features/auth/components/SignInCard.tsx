21fdaf714dcaf76a11f159905382a79c1f55fa17
MarkStudzinskyi <mark.studzinskiy@gmail.com>
2026-02-18 14:50:18 +0000

    close TODO: add toast/sonar notifications for Sign In & Sign Up

--- features/auth/components/SignInCard.tsx
+++ features/auth/components/SignInCard.tsx
@@ -3,9 +3,6 @@ import { Separator } from '@/components/ui/separator';
 
 import { SignInForm } from '../forms/signInForm';
 
-// TODO add toast/sonar notifications Sign In
-//add toast/sonar for success and/or error states for Sign Up to provide feedback to the user on the status of their sign up attempt.
-
 export function SignInCard() {
   return (
     <Card>
--- features/auth/components/signUpCard.tsx
+++ features/auth/components/signUpCard.tsx
@@ -3,9 +3,6 @@ import { Separator } from '@/components/ui/separator';
 
 import { SignUpForm } from '../forms';
 
-// TODO add toast/sonar notifications Sign Up
-//add toast/sonar for success and/or error states for Sign Up to provide feedback to the user on the status of their sign up attempt.
-
 export function SignUpCard() {
   return (
     <Card>
