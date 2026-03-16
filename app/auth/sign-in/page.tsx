f94c89bae4e12c1482ab4b4cb5e9ddda86724758
MarkStudzinskyi <mark.studzinskiy@gmail.com>
2026-03-16 14:13:09 +0000

    fix: add suspense for sign-in page

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import { SignInCard } from '@/features/auth';
 
 export default function SignInPage() {
   return (
     <>
-      <SignInCard />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SignInCard />
+      </Suspense>
     </>
   );
 }
